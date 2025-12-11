d57e00cc090bf2ece316dcecc9d8af1bac102537
Антон Катаев <anton@MacBook-Air-Anton.local>
2025-12-11 14:48:15 +0300

    исправления стилей

--- src/app/shared/components/my-card/my-card.ts
+++ src/app/shared/components/my-card/my-card.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 
 @Component({
   selector: 'app-my-card',
@@ -8,4 +8,5 @@ import {Component, Input} from '@angular/core';
 export class MyCard {
   @Input() header: string | null = null
   @Input() headerAfter: string | null = null
+  @HostBinding('class') hostClass = 'bg-white p-5 rounded-xl shadow-sm block';
 }
